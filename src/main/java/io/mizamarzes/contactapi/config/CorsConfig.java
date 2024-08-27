0bad2ea32f13eb0300c0344bc3fabf49ba10aba7
mizamarzes <mizamarzes@gmail.com>
2024-08-27 01:11:34 -0500

    v1.2.0 - corsConfig process

---
+++ src/main/java/io/mizamarzes/contactapi/config/CorsConfig.java
@@ -0,0 +1,12 @@
+package io.mizamarzes.contactapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    
+    @Bean
+    public CorsFilter
+}
